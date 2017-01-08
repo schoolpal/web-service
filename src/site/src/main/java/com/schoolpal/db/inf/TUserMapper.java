a21ed9f5551531f2747f350c8a7773bd9883234c
Gary Zhu <dinner3000@hotmail.com>
2017-01-08 22:02:32 +0800

    Comment unused methods

--- src/site/src/main/java/com/schoolpal/db/inf/TUserMapper.java
+++ src/site/src/main/java/com/schoolpal/db/inf/TUserMapper.java
@@ -6,22 +6,22 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface TUserMapper {
-    long countByExample(TUserExample example);
-
-    int deleteByExample(TUserExample example);
-    int deleteByPrimaryKey(String cId);
-
-    int insert(TUser record);
-    int insertSelective(TUser record);
-
-    List<TUser> selectByExample(TUserExample example);
-    TUser selectByPrimaryKey(String cId);
+//    long countByExample(TUserExample example);
+//
+//    int deleteByExample(TUserExample example);
+//    int deleteByPrimaryKey(String cId);
+//
+//    int insert(TUser record);
+//    int insertSelective(TUser record);
+//
+//    List<TUser> selectByExample(TUserExample example);
+//    TUser selectByPrimaryKey(String cId);
     TUser selectByLoginName(String cName);
     String selectPasswordByLoginName(String cName);
 
-    int updateByExampleSelective(@Param("record") TUser record, @Param("example") TUserExample example);
-    int updateByExample(@Param("record") TUser record, @Param("example") TUserExample example);
-    int updateByPrimaryKeySelective(TUser record);
-    int updateByPrimaryKey(TUser record);
+//    int updateByExampleSelective(@Param("record") TUser record, @Param("example") TUserExample example);
+//    int updateByExample(@Param("record") TUser record, @Param("example") TUserExample example);
+//    int updateByPrimaryKeySelective(TUser record);
+//    int updateByPrimaryKey(TUser record);
     int updateLastVisitByLoginName(TUser record);
 }
