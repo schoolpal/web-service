02ae067cb95bb0c2bd3c2c5d390a932de8c09553
dinner3000 <dinner3000@hotmail.com>
2017-05-06 10:49:29 +0800

    Update deploy files

--- deploy/docker_image/build.sh
+++ deploy/docker_image/build.sh
@@ -7,24 +7,36 @@ function run(){
 	$@
 }
 
-#WORK DIR SHOULD BE AT $BASE_DIR/deploy
+
 #GIT REPO BASE DIR
-BASE_DIR=../../
-mkdir -p deploy/sqls/1-schema
-mkdir -p deploy/sqls/2-data
-mkdir deploy/packages
+REPO_BASE_DIR=../../
+REPO_DATA_DIR=${REPO_BASE_DIR}/data
+REPO_PKG_DIR=${REPO_BASE_DIR}/src/site/target
+
+#WORK DIR SHOULD BE $REPO_BASE_DIR/deploy/docker_image
+BUILD_DEPLOY_DIR=deploy
+BUILD_SQL_DIR=${BUILD_DEPLOY_DIR}/sqls
+BUILD_SQL_SCHEMA_DIR=${BUILD_SQL_DIR}/1-schema
+BUILD_SQL_DATA_DIR=${BUILD_SQL_DIR}/2-data
+BUILD_PKG_DIR=${BUILD_DEPLOY_DIR}/packages
+
+mkdir -p ${BUILD_SQL_SCHEMA_DIR}
+mkdir -p ${BUILD_SQL_DATA_DIR}
+mkdir -p ${BUILD_PACKAGE_DIR}
 
 #Prepare SQLs
-run "cp -fv ${BASE_DIR}/data/Dump20170323-Structure.sql deploy/sqls/1-schema/"
-run "cp -fv ${BASE_DIR}/data/Dump20170401-Data.sql deploy/sqls/2-data/"
+run "cp -fv ${REPO_DATA_DIR}/*structure*.sql ${BUILD_SQL_SCHEMA_DIR}/"
+run "cp -fv ${REPO_DATA_DIR}/*data*.sql ${BUILD_SQL_DATA_DIR}/"
 
 #Preape package
-run "cp -fv ${BASE_DIR}/src/site/target/*.war deploy/packages/"
+run "cp -fv ${REPO_PKG_DIR}/*.war ${BUILD_PKG_DIR}/"
 
 #Build docker image
-run "docker build -t schoolpal_testbed ."
-run "docker tag schoolpal_testbed dockerhub.internal:5000/schoolpal_testbed"
-run "docker push"
+IMAGE_TAG="schoolpal_testbed"
+DOCKER_HUB="dockerhub.internal:5000"
+run "docker build -t ${IMAGE_TAG}."
+run "docker tag ${IMAGE_TAG} ${DOCKER_HUB}/${IMAGE_TAG}"
+run "docker push ${DOCKER_HUB}/${IMAGE_TAG}"
 #run "docker rm -f testbed"
 #run "docker run -dP --name testbed schoolpal_testbed"
 #run "sleep 3"
