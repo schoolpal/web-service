efb663d8c5bd5059019c1874866a564e49d582f4
dinner3000 <dinner3000@hotmail.com>
2017-05-06 09:58:30 +0800

    Update deploy files

--- deploy/docker_image/build.sh
+++ deploy/docker_image/build.sh
@@ -10,6 +10,9 @@ function run(){
 #WORK DIR SHOULD BE AT $BASE_DIR/deploy
 #GIT REPO BASE DIR
 BASE_DIR=../../
+mkdir -p deploy/sqls/1-schema
+mkdir -p deploy/sqls/2-data
+mkdir deploy/packages
 
 #Prepare SQLs
 run "cp -fv ${BASE_DIR}/data/Dump20170323-Structure.sql deploy/sqls/1-schema/"
@@ -20,6 +23,8 @@ run "cp -fv ${BASE_DIR}/src/site/target/*.war deploy/packages/"
 
 #Build docker image
 run "docker build -t schoolpal_testbed ."
+run "docker tag schoolpal_testbed dockerhub.internal:5000/schoolpal_testbed"
+run "docker push"
 #run "docker rm -f testbed"
 #run "docker run -dP --name testbed schoolpal_testbed"
 #run "sleep 3"
