57971c784847de7ce05bbd5b20810d376032ff39
dinner3000 <dinner3000@hotmail.com>
2017-05-09 08:50:24 +0800

    Move cleanup to last step

--- deploy/jenkins/cleanup.sh
+++ deploy/jenkins/cleanup.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-#docker stop schoolpal_testbed
-docker rm -f schoolpal_testbed
-IMAGES_TO_REMOVE=`docker images -a |grep schoolpal_testbed |awk '{print $3}' |sort -u`
-if [ ! -z "${IMAGES_TO_REMOVE}" ]; then 
-    echo "Remove old images ... "
-    docker rmi -f ${IMAGES_TO_REMOVE}
-else
-    echo "No images to remove"
-fi
-
 docker rm -v $(docker ps -a -q -f status=exited)
 
 docker rmi $(docker images -f "dangling=true" -q)
