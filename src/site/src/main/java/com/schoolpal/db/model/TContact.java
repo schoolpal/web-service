665b1525c8300adb0a7932370912e62b8a6027c3
Gary Zhu <dinner3000@hotmail.com>
2017-05-23 22:12:01 +0800

    Add more properties to contact/list inf result

--- src/site/src/main/java/com/schoolpal/db/model/TContact.java
+++ src/site/src/main/java/com/schoolpal/db/model/TContact.java
@@ -7,14 +7,42 @@ public class TContact {
 
     private String leadsId;
 
-    private String approach;
+    private String approachName;
     private String approachId;
 
     private Date datetime;
 
+    private String executiveName;
     private String executiveId;
 
-    private String summary;
+    private String orgName;
+    private String orgId;
+    
+    public String getExecutiveName() {
+		return executiveName;
+	}
+
+	public void setExecutiveName(String executiveName) {
+		this.executiveName = executiveName;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+	public String getOrgId() {
+		return orgId;
+	}
+
+	public void setOrgId(String orgId) {
+		this.orgId = orgId;
+	}
+
+	private String summary;
 
     public String getId() {
         return id;
@@ -32,12 +60,12 @@ public class TContact {
         this.leadsId = leadsId == null ? null : leadsId.trim();
     }
 
-    public String getApproach() {
-        return approach;
+    public String getApproachName() {
+        return approachName;
     }
 
-    public void setApproach(String approach) {
-        this.approach = approach == null ? null : approach.trim();
+    public void setApproachName(String approachName) {
+        this.approachName = approachName == null ? null : approachName.trim();
     }
 
     public String getApproachId() {
