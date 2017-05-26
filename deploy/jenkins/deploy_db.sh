4b64e7fbf0c0e6395744d4257a969230b7546d92
dinner3000 <dinner3000@hotmail.com>
2017-05-26 20:49:01 +0800

    Update deploy files

--- deploy/jenkins/deploy_db.sh
+++ deploy/jenkins/deploy_db.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+DOCKER_HUB=dockerhub.internal:5000
+
 docker rm -f db
-docker -d --restart=always -p 3306:3306 --name db dockerhub.internal:5000/db:latest
+docker pull ${DOCKER_HUB}/db:latest
+docker run -d --restart=always -p 3306:3306 --name db ${DOCKER_HUB}/db:latest
 docker ps -a
