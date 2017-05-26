071b9ae7624bc76d0628e60994058c1f676c5a43
dinner3000 <dinner3000@hotmail.com>
2017-05-26 23:27:44 +0800

    Update deploy files

--- deploy/jenkins/deploy_docker_image.sh
+++ deploy/jenkins/deploy_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z $4 ]; then 
-    echo "Usage: $0 [DOCKER_HUB] [IMAGE] [PORT] [NAME]"
+    echo "Usage: $0 [DOCKER_HUB] [IMAGE] [PORT] [NAME] [LINKS]"
     exit 0
 fi
 
@@ -9,8 +9,11 @@ DOCKER_HUB=$1
 IMAGE=$2
 PORT=$3
 NAME=$4
+if [ ! -z "$5" ]; then
+    LINKS="$5"
+fi
 
 docker rm -f ${NAME}
 docker pull ${DOCKER_HUB}/${IMAGE}:latest
-docker run -d --restart=always -p ${PORT} --name ${NAME} ${DOCKER_HUB}/${IMAGE}:latest
+docker run -d --restart=always -p ${PORT} ${LINKS} --name ${NAME} ${DOCKER_HUB}/${IMAGE}:latest
 docker ps -a
