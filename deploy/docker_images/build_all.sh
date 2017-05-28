8f40d7928639ecbcf2a30d12cf2d8b3315297419
dinner3000 <dinner3000@hotmail.com>
2017-05-28 12:20:42 +0800

    Update nginx conf

--- deploy/docker_images/build_all.sh
+++ deploy/docker_images/build_all.sh
@@ -12,7 +12,7 @@ fi
 
 DOCKER_HUB=$1
 #IMAGE_NAMES="supervisor sshd redis-singleton mariadb tomcat9_jre8"
-IMAGE_NAMES="supervisor redis-singleton mariadb tomcat9_jre8"
+IMAGE_NAMES="supervisor redis-singleton mariadb tomcat9_jre8 nginx"
 
 for NAME in ${IMAGE_NAMES}; do
     ./build_one.sh ${DOCKER_HUB} ${NAME}
